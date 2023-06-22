ad33a95f4abc28de2ce037b94b2f5011a440683e
Synthia250 <uwasynthia@gmail.com>
2023-06-21 20:17:40 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,10 @@
 #include "main.h"
+
 /**
  * main - Entry point
- * Description: 'prints _putchar to stdout'
  * Return: always 0
  */
+
 int main(void)
 {
 	_putchar('_');
--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,16 +7,17 @@
  */
 int main(void)
 {
-	int county;
+	int count;
 	unsigned long fibl = 0, fib2 = 1, sum;
 	unsigned long fib1_half1, fib1_half2, fib2_half1, fibi2_half2;
 	unsigned long half1, half2;
+	
 	for (count = 0; count < 92; count++)
 	{
 		sum = fib1 + fib2;
 		printf("%lu, ", sum);
 		fib1 = fib2
-			fib2 = sum;
+		fib2 = sum;
 	}
 	fib1_half1 = fib1 / 10000000000;
 	fib2_half1 =fib2 / 10000000000;
@@ -32,10 +33,10 @@ int main(void)
 			half2 %= 10000000000;
 		}
 		printf("%lu%lu", half1, half2);
-		if (count !+ 98)
-			printf(",");
+		if (count != 98)
+		printf(",");
 		fib1_half1 = fib2_half1;
-		fibi1_falf2 = fib2_half2;
+		fib1_half2 = fib2_half2;
 		fib2_half1 = half1;
 		fib2_half2 = half2;
 	}
